b7613e0dc9b6b320ceb6da17c8fb650859315e81
TheLvableMrFish <paganochristians@gmail.com>
2024-11-06 04:06:54 -0600

    started seperating a part of the character from the main scrip.js

---
+++ js/character.js
@@ -0,0 +1,28 @@
+const characterDirectionList ={
+    "up" : [
+        { sourceX: 76, sourceY: 34},
+        { sourceX: 60, sourceY: 34},
+        { sourceX: 76, sourceY: 34},
+        { sourceX: 94, sourceY: 34},
+    ],
+    "right" : [
+        { sourceX: 144, sourceY: 34},
+        { sourceX: 162, sourceY: 34},
+        { sourceX: 144, sourceY: 34},
+        { sourceX: 162, sourceY: 34},
+    ],
+    "down" : [
+        { sourceX: 25, sourceY: 34},
+        { sourceX: 9, sourceY: 34},
+        { sourceX: 25, sourceY: 34},
+        { sourceX: 42, sourceY: 34},
+    ],
+    "left" : [
+        { sourceX: 110, sourceY: 34},
+        { sourceX: 126, sourceY: 34},
+        { sourceX: 110, sourceY: 34},
+        { sourceX: 126, sourceY: 34},
+    ],
+}
+
+export default characterDirectionList
